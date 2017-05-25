b60e8f6e2fbf16f0d0657d60c62bcc53638c3fae
Hanson_C <hansoncoder@gmail.com>
2017-05-25 16:07:20 +0800

    修改WiFi

--- WiFi-Password-Find/myScript/wifi.py
+++ WiFi-Password-Find/myScript/wifi.py
@@ -4,38 +4,8 @@
 import ftplib
 import os
 import sys
+import Tools
 
-#---------- 数组中文乱码 -----------#
-
-# 将 "\\U90fd" 转换 "\u90fd" (数组将 "\\U90fd" 字符串输出为中文，必须转为 "\u90fd" 格式)
-def getUnicodeStrFromOther(string):
-    if "\\\\U" in string:
-        return string.replace("\\\\U","\\u")        
-    elif "\\U" in string:
-        return string.replace("\\U","\\u")
-    return string
-
-# 将 "\u90fd\u662f\u7231\u7684" 转换为中文输出
-def getUTF8FromUnicodeStr(unicodeString):
-    return unicodeString.decode('unicode_escape').encode('utf-8')
-
-# 将数组中 item 包含 "\\U90fd" 转换 "\u90fd" 
-def getItemAtArrayItem(item):
-    if "\\U" in item:
-        item = getUnicodeStrFromOther(item)
-        item = getUTF8FromUnicodeStr(item)
-    return item
-#---------- 数组中文乱码 -----------#
-
-
-def getCurrentPath():
-    return os.path.dirname(__file__)
-
-def getPardir():
-    return os.path.abspath(os.path.join(getCurrentPath(), os.pardir))
-
-def execute(command):
-    return os.popen(command).read()
 
 # 读取整数
 def readInt():
@@ -60,20 +30,24 @@ def readIndex(SSIDList):
 
 # 获取 WiFi 名称
 def getWiFiNameList():
-    SSIDListStr = execute('defaults read /Library/Preferences/SystemConfiguration/com.apple.airport.preferences | grep SSIDString')
+    SSIDListStr = Tools.execute('defaults read /Library/Preferences/SystemConfiguration/com.apple.airport.preferences | grep SSIDString')
     temp = SSIDListStr.replace("SSIDString = ","").replace(";",",")
-    SSIDList = temp.split(",")
+    tempSSIDList = temp.split(",")
     # 处理查询结果
-    if len(SSIDList) > 0 :
-        SSIDList.pop()
-    for index,SSIDName in enumerate(SSIDList):
-        SSIDList[index] = SSIDName.strip()
+    if len(tempSSIDList) > 0 :
+        tempSSIDList.pop()
+    
+    # 去除一些无用字符串
+    SSIDList = []
+    for index,SSIDName in enumerate(tempSSIDList):
+        # if (SSIDName.find("\\\\U") == -1): # 去除中文
+            SSIDList.append(SSIDName.strip().replace("\"",""))
     return SSIDList;
 
 # 获取 WiFi 密码
 def getWiFiPasswd(wifiName) :
     output = os.popen('security find-generic-password -wa ' + wifiName)
-    os.system('/usr/bin/osascript ' + getCurrentPath() + '/window.AppleScript')
+    os.system('/usr/bin/osascript ' + Tools.getCurrentPath() + '/window.AppleScript')
     wifiPasswd = output.read()
     return wifiPasswd
 
@@ -81,7 +55,7 @@ def getWiFiPasswd(wifiName) :
 def printWiFIName(List):
     print "----------------------------------"
     for index,item in enumerate(List):
-        print "|  " + str(index+1) + '.WiFi名称：' + getItemAtArrayItem(item)
+        print "|  " + str(index+1) + '.WiFi名称：' + Tools.getItemAtArrayItem(item)
     print "----------------------------------"
 
 # 写入文件
@@ -91,25 +65,25 @@ def writeToFile(file,text):
     file_object.close()
 
 def printfResult(ssid,passwd):
-    text = "您可能存在以下情况：\n 1.WiFi名称：" + getItemAtArrayItem(wifiName) + " 没有密码！\n 2.没有配置window.AppleScript脚本！"
+    text = "您可能存在以下情况：\n 1.WiFi名称：" + Tools.getItemAtArrayItem(wifiName) + " 没有密码！\n 2.没有配置 window.AppleScript 脚本！"
     if len(passwd) != 0:
         text = "WiFi名称：" + ssid + "\nWiFi密码："+ passwd
-    filePath = getPardir() + "/wifi.txt"
+    filePath = Tools.getPardir() + "/wifi.txt"
     writeToFile(filePath, text)
     os.popen("open " + filePath)
 
 ## -------- 进入主题 ------------- ##
 
 # 获取WiFi名称
-
 SSIDList = getWiFiNameList()
 # 读取用户输入
 selectIndex = readIndex(SSIDList)
 
 # 通过 WiFi 名称获取WiFi密码
 wifiName = SSIDList[selectIndex]
-wifiPasswd = getWiFiPasswd(wifiName)
+wifiPasswd = getWiFiPasswd(Tools.getItemAtArrayItem(wifiName))
 
 # 写入文件
 printfResult(wifiName,wifiPasswd)
 
+## -------- end ------------- ##
